02ec3d84a6428c49e144b2e017b367116fd24e3f
Will Mavis <wlrdq@yahoo.com>
2025-04-04 17:10:41 -0500

    Jog and idle

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -8,13 +8,13 @@ public class ThirdPersonCamera : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()   
     {
         Vector3 viewDirection = player.transform.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        player.transform.forward = viewDirection.normalized;
+        player.transform.forward = new Vector3(viewDirection.normalized.x, player.transform.forward.y, viewDirection.normalized.z);
     }
 }
--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -4,6 +4,8 @@ public class ThirdPersonController : MonoBehaviour
 {
     public float movementSpeed = 5;
     Rigidbody rb;
+    Animator animator;
+
 
     float horizontalInput;
     float verticalInput;
@@ -12,16 +14,31 @@ public class ThirdPersonController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
 
         Vector3 moveDirection = (transform.forward * verticalInput + transform.right * horizontalInput).normalized;
 
         rb.AddForce(moveDirection * movementSpeed, ForceMode.Force);
+
+        // Animation
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+
+        if(flatVel.magnitude > movementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * movementSpeed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
+
+        float velocity = flatVel.magnitude;
+
+        animator.SetFloat("Velocity", velocity);
     }
 }

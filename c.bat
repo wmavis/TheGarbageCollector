6044b0b4d1beeab326b02ec83ac1ca0260b24341
Will Mavis <wlrdq@yahoo.com>
2025-04-10 16:02:16 -0500

    Update c.bat to commit changes

--- c.bat
+++ c.bat
@@ -2,4 +2,7 @@ cd \Users\WillardPC\github\wmavis.github.io\TheGarbageCollector\
 del /q /s Build
 del /q /s TemplateData 
 xcopy /y /s ..\..\TheGarbageCollector\Build\* .
+git add .
+git commit -m %*
+git push
 cd \Users\WillardPC\github\TheGarbageCollector\
